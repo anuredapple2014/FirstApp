c8a03491f9c4a822642ae7dc0e27fdf93e4c25f9
anuredapple2014 <anurag@redappletech.com>
2022-09-14 15:59:31 +0530

    commit1

---
+++ src/screens/GooglePlaces.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GooglePlaces = () => {
+  return (
+    <View>
+      <Text>GooglePlaces</Text>
+    </View>
+  )
+}
+
+export default GooglePlaces
+
+const styles = StyleSheet.create({})
