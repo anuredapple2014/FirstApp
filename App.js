182aa505c1b56433b864d6ea22a41bea9a0c93af
anuredapple2014 <anurag@redappletech.com>
2022-09-19 18:50:25 +0530

    commit 6

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Pickup from './src/screens/Pickup'
+import React, { useState } from 'react'
+import DropDown from './src/screens/DropDown'
+//import Pickup from './src/screens/Pickup'
 //import OrderStatus from './src/screens/OrderStatus'
 //import SelectLocation from './src/screens/SelectLocation'
 //import DeliveryDetails from './src/screens/DeliveryDetails'
@@ -11,10 +12,32 @@ import Pickup from './src/screens/Pickup'
 //import MobileNumber from './src/screens/MobileNumber'
 
 //import Location from './src/screens/Location'
-
 const App = () => {
+  const [selectedItem,setSelectedItem] = useState(null)
+  const onSelect = (item) =>{
+       setSelectedItem(item)
+  }
+  const fruits =[
+    {
+    id:1,
+    name:'Mango'
+    },
+    {
+        id:2,
+        name:'Banana'
+        },
+        {
+            id:3,
+            name:'Apple'
+            }
+]
+
   return (
-    <Pickup/>
+    <DropDown 
+    value={selectedItem}
+    data={fruits}
+    onSelect={onSelect}
+    />
   )
 }
 
---
+++ src/screens/DropDown.js
@@ -0,0 +1,72 @@
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
+import React,{useState} from 'react'
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+  } from 'react-native-responsive-screen';
+  import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+
+const DropDown = ({
+    data =[],
+    value={},
+    onSelect = () =>{}
+}) => {
+   console.log("Value is =====>",!!value)
+   const[showOption, setShowOption] = useState(false)
+   const onSelectedItem = (val) =>{
+    setShowOption(false)
+    onSelect(val)
+   }
+  return (
+    <View style={styles.container}>
+        <TouchableOpacity
+        style={styles.dropdownStyle}
+         activeOpacity={0.8}
+         onPress={()=>setShowOption(!showOption)}>
+            <Text>{!! value? value?.name : 'Choose as Option'}</Text>
+            <FontAwesome5
+            style={{fontSize: hp('1.8%'), color: '#808080'}}
+            name="location-arrow"
+            color="#ffffff"
+            size={hp('2%')}
+          />
+        </TouchableOpacity>
+        {showOption &&(<View>
+      {data.map((val,i)=>{
+        return (
+            <TouchableOpacity 
+            key={String(i)} 
+            onPress={()=>onSelectedItem(val)}
+            style={{backgroundColor: value.id == val.id ? 'orange' : 'white',
+                     width:wp('70%'),height:hp('4%'),alignSelf:'center'}}>
+            <Text  style={{alignSelf:'flex-start'}}> {val.name}</Text>
+            </TouchableOpacity>
+        )
+      })}
+      </View>)}
+    </View>
+  )
+}
+
+export default DropDown
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+       
+    },
+    dropdownStyle:{
+        backgroundColor:'pink',
+       justifyContent:'space-between',
+       paddingHorizontal:wp('2%'),
+       borderRadius:hp('1%'),
+       width:wp('70%'),
+       height:hp('5%'),
+       flexDirection:"row",
+       alignItems:'center',
+       alignSelf:'center',
+       marginTop:hp('5%')
+       //justifyContent:'center'
+    }
+})
